8fe57fd8a6ec9eaf12d8ea7fc8870cacbd7839aa
Mu'adz Hudzaifah <muadzhudzaifah511@gmail.com>
2026-09-08 15:44:04 +0700

    chore(release): bump version to 2.0.0 and add automated PyPI release workflow

--- fluxload/__init__.py
+++ fluxload/__init__.py
@@ -1,6 +1,6 @@
 """FluxLoad: A simple, modern file server."""
 
-__version__ = "1.2.9"
+__version__ = "2.0.0"
 
 # Export create_app from advanced_server as default
 from .advanced_server import create_app
